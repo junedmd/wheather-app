bd5aa8ebc3aab09f6d3cbe1b6fe3f86b679219fb
junedmd <mohammadjuned074@gmail.com>
2023-10-05 01:31:13 +0530

    initial secnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+
+ const [ weatherData ,setWeatherData] =useState({});
+ const [city ,setCity] =useState("pune")
+    async function loadWhetherData(){
+      let response =" "
+      try {
+        response =  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f652964084c552e8c0492237a3fabd9c`);
+        setWeatherData(response.data);
+      }
+      catch(error){
+        console.log(response.data);
+      }
+    
+  
+  
+  }
+
+  useEffect(()=>{
+    loadWhetherData();
+  })
+
+  useEffect(()=>{
+    loadWhetherData();
+  } ,[city])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <div className='container'>
+        <h1 className='h1'> Wheather App: </h1>
+      <div className='input-div'>
+          <input  placeholder=' Search city' className=' inputbox' type='text' value={city} onChange={(e)=>{
+            setCity(e.target.value);
+          }}/>
+          <h1 className='p'> {weatherData?.name}</h1>
+          <h1 className='pa'> {(weatherData?.main?.temp-273).toFixed(2)} °C</h1>
+      </div>
+
+
+   </div>
+   </>
   );
 }
 
